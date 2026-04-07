8ed057e5b09506ebb968355cedc415711caa53af
YaFred <yafred@users.noreply.github.com>
2026-04-07 21:55:10 +0200

    add log

--- src/real3d/scene.ts
+++ src/real3d/scene.ts
@@ -83,6 +83,7 @@ export function start3D(sceneRoot: HTMLElement, config: Config): Api {
   }
 
   function setAllowInteractionForColors(config: Config) {
+    console.log('Setting allow interaction for colors based on config:', config);
     if (config?.turnColor) {
       const isWhiteTurn = config.turnColor === 'white';
       const isMyTurn =
@@ -91,6 +92,7 @@ export function start3D(sceneRoot: HTMLElement, config: Config): Api {
         config.movable?.color === 'both';
       interactionController.setAllowWhiteInteraction(isWhiteTurn && isMyTurn);
       interactionController.setAllowBlackInteraction(!isWhiteTurn && isMyTurn);
+      console.log(`Turn color: ${config.turnColor}, isMyTurn: ${isMyTurn}, allowWhiteInteraction: ${isWhiteTurn && isMyTurn}, allowBlackInteraction: ${!isWhiteTurn && isMyTurn}`); 
     }
   }
 
